7bfe8797de6bc6fb0c4e15ea3a7dbebce3a69ec9
Sabrina Mares <sabrsgomes@gmail.com>
2020-12-15 19:31:55 -0300

    responsividade

--- dados/allCorres.js
+++ dados/allCorres.js
@@ -5,7 +5,7 @@ const allCorres = [
         titulo: "ajuliacosta",
         bioImg1: "https://static.wixstatic.com/media/77d60e_3fe98ae60c044df483e4a00215b1f5b0~mv2_d_3264_4248_s_4_2.gif",
         bioImg2: "https://static.wixstatic.com/media/77d60e_b54b9606146342df8ecddb196eb44c80~mv2.png/v1/fill/w_478,h_578,al_c,q_85,usm_0.66_1.00_0.01/77d60e_b54b9606146342df8ecddb196eb44c80~mv2.webp",
-        ul: "<li>São Paulo/SP</li><li>mulher preta empreendedora</li><li>dona da marca <span class='span__3'>ajuliacostashop</span>, onde produz roupas de forma independente</li><li>rima e lança uns rap pesado de preta enjoada</li><li>fortalece a autoestima das mina</li><li>trampo de preta pra preta</li>",
+        ul: "<li>São Paulo/SP</li><li>mulher preta empreendedora</li><li>dona da marca <span class='span__3'>ajuliacostashop</span></li><li>rima e lança uns rap pesado de preta enjoada</li><li>fortalece a autoestima das mina</li><li>trampo de preta pra preta</li>",
         bannerImg1: "https://i1.wp.com/uc-br.com/wp-content/uploads/2020/06/Captura-de-Tela-2020-06-17-a%CC%80s-21.21.46.png?fit=1271%2C678&ssl=1",
         bannerImg2:"https://3.bp.blogspot.com/-OX03JrrFvdM/V2R-Kq9nB3I/AAAAAAAAzxE/b_zjdO5iWwoLcEjGAOeZcZQkmQ0pnSKfQCLcB/w1200-h630-p-k-no-nu/13323609_1716703465250965_5110768966165624211_o.jpg",
         link1: "https://www.instagram.com/ajuliacosta/",
